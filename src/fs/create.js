8ad3c475511072397be8a44ed5449a6ed701b2e2
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2024-01-24 19:34:45 +0300

    feat: implement fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from "fs/promises";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const ErrorMsg = "FS operation failed";
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+
+  try {
+    await fs.access(filePath);
+    throw new Error(ErrorMsg);
+  } catch (err) {
+    if (err.message === ErrorMsg) throw err;
+
+    await fs.writeFile(filePath, "I am fresh and young");
+  }
 };
 
 await create();
