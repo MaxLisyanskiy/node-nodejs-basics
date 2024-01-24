24e3e491686fecda0988ecb72ff2b19c3f1ad03a
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2024-01-24 23:03:03 +0300

    feat: implement fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import fs from "fs/promises";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const ErrorMsg = "FS operation failed";
+
 const copy = async () => {
-    // Write your code here 
+  const folderPath = path.join(__dirname, "files");
+  const copyFolderPath = path.join(__dirname, "files_copy");
+
+  try {
+    await fs.access(folderPath);
+    await fs.mkdir(copyFolderPath);
+  } catch (error) {
+    throw new Error(ErrorMsg);
+  }
+
+  try {
+    await fs.cp(folderPath, copyFolderPath, { recursive: true });
+  } catch (error) {
+    throw new Error(ErrorMsg);
+  }
 };
 
 await copy();
