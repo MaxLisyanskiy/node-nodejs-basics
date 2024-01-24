c98f2b020fca37e08419f34b085c8bb2da7fcaac
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2024-01-24 22:30:07 +0300

    feat: implement fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import fs from "fs/promises";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const ErrorMsg = "FS operation failed";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fileToRead.txt");
+
+  try {
+    const text = await fs.readFile(filePath, "utf8");
+    console.log(text);
+  } catch (err) {
+    throw new Error(ErrorMsg);
+  }
 };
 
 await read();
