597c8da65cbe70dd01d3b3ba2f7a0a307d93e3d0
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2024-01-25 19:25:03 +0300

    feat: implement src/streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,26 @@
+import { Transform } from "stream";
+
+const ErrorMsg = "FS operation failed";
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    const transformStream = new Transform({
+      transform(chunk, _, callback) {
+        this.push(chunk.toString().split("").reverse().join(""));
+        callback();
+      },
+    });
+
+    console.log(
+      "Enter the text below and press 'Enter'. (Press CTRL+C to close terminal)"
+    );
+
+    process.stdin.pipe(transformStream);
+
+    transformStream.pipe(process.stdout);
+  } catch (err) {
+    throw new Error(ErrorMsg);
+  }
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
+import fs from "fs";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const ErrorMsg = "FS operation failed";
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fileToWrite.txt");
+
+  try {
+    const stream = fs.createWriteStream(filePath);
+    console.log(
+      "Enter the text below and press 'Enter'. (Press CTRL+C to close terminal)"
+    );
+    process.stdin.pipe(stream);
+  } catch (err) {
+    throw new Error(ErrorMsg);
+  }
 };
 
 await write();
