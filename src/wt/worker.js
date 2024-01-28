359de61c897a8ab96b8a3f8098d55fc9f0f178aa
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2024-01-28 14:50:00 +0300

    feat: implement src/wt

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { workerData, parentPort } from "worker_threads";
+
+const ErrorMsg = "FS operation failed";
+
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  try {
+    const result = nthFibonacci(workerData);
+    parentPort.postMessage(result);
+  } catch (err) {
+    throw new Error(ErrorMsg);
+  }
 };
 
 sendResult();
